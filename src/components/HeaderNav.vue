9cedcc821258076f4dbf2011323a964fe6ba553f
honshin4n1 <million_films.sh@icloud.com>
2020-10-26 18:23:57 +0900

    Update HeaderNav.vue

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,4 +1,15 @@
 <template>
+  <nav class="header-nav">
+    <div class="nav-link">
+      <router-link to="/" class="link" active-class="link--active" exact>home</router-link>
+    </div>
+    <div class="nav-link">
+      <router-link to="/about" class="link" active-class="link--active" exact>about</router-link>
+    </div>
+    <div class="nav-link">
+      <router-link to="/skills" class="link" active-class="link--active" exact>skills</router-link>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -8,15 +19,25 @@ export default {
 </script>
 
 <style>
-  .main-container {
-    height: 100%;
-    width: 100%;
-  }
   .header-nav {
     height: 50px;
-    width: 50%;
+    width: 100%;
     display: flex;
     justify-content: center;
+    margin: 0 auto;
+    position: absolute;
+    bottom: 50px;
+    box-shadow: 0 2px 4px -1px rgba(0,0,0,.2), 0 4px 5px 0 rgba(0,0,0,.14), 0 1px 10px 0 rgba(0,0,0,.12);
+  }
+  .nav-link {
+    margin: 10px;
+  }
+  .link {
+    text-decoration: none;
+    color: dimgray;
+  }
+  .link--active {
+    color: darkgrey;
   }
 
 </style>

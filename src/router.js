63c7defa6a0ee68c95d587acf8e6ab776c5acd15
honshin4n1 <million_films.sh@icloud.com>
2020-10-26 18:22:37 +0900

    created router.js

--- src/router.js
+++ src/router.js
@@ -1,8 +1,12 @@
-import Vue from 'vue';
-import Router from 'vue-router';
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+import About from "./views/About.vue"
+import Skills from "./views/Skills.vue";
 
 Vue.use(Router);
 
-new Router({
-  routes: [{}, {},{}]
+export default new Router({
+  mode: 'history',
+  routes: [{path: '/', component: Home}, {path: '/about', component: About}, {path: '/skills', component: Skills}]
 });

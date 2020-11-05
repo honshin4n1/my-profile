0e9b239f850cb5dee041cd6e37ddb7ca35e8fa5f
honshin4n1 <million_films.sh@icloud.com>
2020-11-05 18:53:47 +0900

    Update Contact.vue

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,61 +1,30 @@
 <template>
   <div class="contact">
-    <transition name="info" appear>
+    <!-- <transition name="info" appear>
       <div class="contact-info">
         <h3>只今、メンテナンス中です。</h3>
       </div>
-    </transition>
+    </transition> -->
     <div class="main">
       <div class="title">
         <h1><font-awesome-icon icon="envelope"></font-awesome-icon></h1>
         <h1>Contact</h1>
       </div>
-      <div class="contact-form">
-        <form name="contact" method="POST">
-          <div class="form-group">
-            <label for="name">名前</label>
-            <br>
-            <input id="name" type="text" v-model.lazy="contactData.name">
-            <p>{{contactData.name}}</p>
-          </div>
-          <div class="form-group">
-            <label for="email">Email</label>
-            <br>
-            <input id="email" type="email" v-model.lazy="contactData.email">
-            <p>{{contactData.email}}</p>
-          </div>
-          <div class="form-group">
-            <label for="comment">内容</label>
-            <br>
-            <textarea id="comment" cols="30" rows="5" v-model.lazy="contactData.comment"></textarea>
-            <p>{{contactData.comment}}</p>
-          </div>
-          <div class="form-group">
-            <button type="submit">送信</button>
-          </div>
-        </form>
-      </div>
+      <ContactForm></ContactForm>
     </div>
     <Footer></Footer>
   </div>
 </template>
 
 <script>
+import ContactForm from "@/components/ContactForm.vue";
 import Footer from "@/components/Footer.vue";
 // import axios from "axios";
 
 export default {
-  data() {
-    return {
-      contactData: {
-        name: '',
-        email: '',
-        comment: ''
-      }
-    };
-  },
   components: {
-    Footer
+    Footer,
+    ContactForm
   }
 }
 </script>
@@ -64,31 +33,14 @@ export default {
 .contact {
   height: 100%;
   width: 100%;
+  padding-bottom: 200px;
 }
 .main {
   height: 100%;
   width: 80%;
   margin: 0 auto;
 }
-.contact-form {
-  width: 80%;
-  margin: 50px auto;
-  padding-top: 30px;
-  padding-bottom: 30px;
-  border: 1px solid darkgrey;
-  border-radius: 4px;
-  box-shadow: 0 2px 4px -1px rgba(0,0,0,.2), 0 4px 5px 0 rgba(0,0,0,.14), 0 1px 10px 0 rgba(0,0,0,.12);
-}
-.contact-info {
-  height: 100%;
-  width: 100%;
-  line-height: 200%;
-  text-align: center;
-  opacity: 0.8;
-  position: fixed;
-  top: 0;
-  background-color: black;
-}
+
 h3 {
   width: 50%;
   margin: 0;
@@ -97,36 +49,12 @@ h3 {
   top: 30%;
   left: 25%;
 }
-.form-group {
-  width: 60%;
-  margin: auto;
-}
 
-input {
-  width: 100%;
-  height: 40px;
-  outline: none;
-  padding: 5px;
-  border: 1px solid darkgrey;
-}
-textarea {
-  width: 100%;
-  outline: none;
-  padding: 5px;
-  border: 1px solid darkgrey;
-}
 .info-enter, .info-leave-to {
   opacity: 0;
 }
 .info-enter-active, .info-leave-active {
   transition: opacity 5s;
 }
-button {
-  height: 40px;
-  width: 100%;
-  outline: none;
-  border: 1px solid darkgrey; 
-  border-radius: 4px;
-  background-color: darkseagreen;
-}
+
 </style>

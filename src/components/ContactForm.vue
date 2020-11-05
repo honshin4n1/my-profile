c88d2e7afde81fa40ae8fcc0151483a55f3b883f
honshin4n1 <million_films.sh@icloud.com>
2020-11-05 18:59:25 +0900

    Update ContactForm.vue

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -80,16 +80,7 @@ export default {
   border-radius: 4px;
   box-shadow: 0 2px 4px -1px rgba(0,0,0,.2), 0 4px 5px 0 rgba(0,0,0,.14), 0 1px 10px 0 rgba(0,0,0,.12);
 }
-.contact-info {
-  height: 100%;
-  width: 100%;
-  line-height: 200%;
-  text-align: center;
-  opacity: 0.8;
-  position: fixed;
-  top: 0;
-  background-color: black;
-}
+
 .form-group {
   width: 80%;
   margin: auto;

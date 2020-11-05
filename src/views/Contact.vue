6c48efd44e38f93b40a7bc84f6fbbe1271f7c0b0
honshin4n1 <million_films.sh@icloud.com>
2020-11-05 18:59:29 +0900

    Update Contact.vue

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="contact">
-    <!-- <transition name="info" appear>
+    <transition name="info" appear>
       <div class="contact-info">
         <h3>只今、メンテナンス中です。</h3>
       </div>
-    </transition> -->
+    </transition>
     <div class="main">
       <div class="title">
         <h1><font-awesome-icon icon="envelope"></font-awesome-icon></h1>
@@ -48,6 +48,17 @@ h3 {
   position: absolute;
   top: 30%;
   left: 25%;
+}
+
+.contact-info {
+  height: 100%;
+  width: 100%;
+  line-height: 200%;
+  text-align: center;
+  opacity: 0.8;
+  position: fixed;
+  top: 0;
+  background-color: black;
 }
 
 .info-enter, .info-leave-to {

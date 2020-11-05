f2261b0bf27a5dd452517df428d70824d0a0f97d
honshin4n1 <million_films.sh@icloud.com>
2020-11-05 18:53:45 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,10 +7,12 @@ import { fab } from '@fortawesome/free-brands-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import VueTyperPlugin from 'vue-typer'
+import Vuelidate from 'vuelidate'
 
 
 library.add(fab, far, fas)
 Vue.use(VueTyperPlugin)
+Vue.use(Vuelidate)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false

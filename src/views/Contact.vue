c8cbb5bf6d12a3a37b04fc98ad263dbbf3a40242
honshin4n1 <million_films.sh@icloud.com>
2020-11-06 13:37:01 +0900

    Update Contact.vue

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="contact">
-    <transition name="info" appear>
+    <!-- <transition name="info" appear>
       <div class="contact-info">
         <h3>只今、メンテナンス中です。</h3>
       </div>
-    </transition>
+    </transition> -->
     <div class="main">
       <div class="title">
         <h1><font-awesome-icon icon="envelope"></font-awesome-icon></h1>
         <h1>Contact</h1>
+      </div>
+      <div class="infomation-text">
+        <p>ご覧いただいてありがとうございます。現在転職活動中です。</p>
+        <p>気になること、お手伝いできることがあれば、お気軽にお問合せください。</p>
       </div>
       <ContactForm></ContactForm>
     </div>
@@ -48,6 +52,12 @@ h3 {
   position: absolute;
   top: 30%;
   left: 25%;
+}
+.infomation-text {
+  margin: 50px;
+}
+p {
+  font-size: 18px;
 }
 
 .contact-info {
